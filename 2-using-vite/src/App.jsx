bd4446e553916c494496b2962c714bc8279dd582
musawerxd <hunnyshah0012@gmail.com>
2026-03-25 20:21:18 +0500

    experiment with variable and element injection in main.jsx

--- 2-using-vite/src/App.jsx
+++ 2-using-vite/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+
+function App() {
+  let username = "Musawer" // variable injection 
   return (
     <>
-      <h1>Hi, I am Musawer</h1>
+      <h1>Hi, I am {username} </h1>
     </>
+    // this is evaluated expression, means it displays final evalued outcome , we can perform operationslike (+ , - , * etc , ternaary, boolean ) but cannnot perform if, for, while , as they are NOT allowed
   )
 }
 
--- 2-using-vite/src/main.jsx
+++ 2-using-vite/src/main.jsx
@@ -2,9 +2,45 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
+
+
+function FnInMain() {
+  return (
+
+    <h2>I am doing CS from UAF</h2>
+
+  )
+}
+// function FnInMain() {   //2
+//   return <h2>I am doing CS from UAF</h2>
+// }
+
+const elementInMain = (
+  <a href="google.com" target='_blank'> Google </a>
+)
+
+const username = " global Username"
+
+const anotherelementInMain = React.createElement("a", { href: "https://www.facebook.com/", target: "_blank" }, "this is made using React.createElement, click to facebook", username)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+  // <>
+  //   <App />
+  //   <FnInMain />
+  // </>
+
+  // FnInMain()
+
+  // <FnInMain />  //2
+
+  // elementInMain
+
+  // anotherelementInMain
+
+  <>
     <App />
-  </StrictMode>,
+    <FnInMain />
+  </>
 )
